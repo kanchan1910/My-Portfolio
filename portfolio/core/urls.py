b74bea3c30dc6d0997df747d5fc70310003d88da
Kanchan Jeswani <kanchanjeswani2000@gmail.com>
2021-05-10 04:02:29 +0530

    Uploaded files for the protfolio

--- portfolio/core/urls.py
+++ portfolio/core/urls.py
@@ -4,6 +4,13 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('edu', views.education, name="edu"),
     path('skills', views.skills, name="skills"),
+    path('programming', views.programming, name="programming"),
+    path('projects', views.projects, name="projects"),
+    path('ex', views.experience, name="ex"),
+    path('resume', views.resume, name="resume"),
+    path('github', views.github, name="github"),
+    path('certi', views.certi, name="certi"),
     path('contact', views.contact, name="contact"),
 ]
--- portfolio/core/views.py
+++ portfolio/core/views.py
@@ -6,9 +6,40 @@ def home(request):
 	context = {'home':'active'}
 	return render(request, 'core/home.html', context)
 
+def education(request):
+	context = {'edu':'active'}
+	return render(request, 'core/education.html', context)
+
+
 def skills(request):
 	context = {'skill':'active'}
 	return render(request, 'core/skill.html', context)
+
+def programming(request):
+	context = {'pro':'active'}
+	return render(request, 'core/programming.html', context)
+
+def projects(request):
+	context = {'projects':'active'}
+	return render(request, 'core/projects.html', context)
+
+def experience(request):
+	context = {'ex':'active'}
+	return render(request, 'core/experience.html', context)
+
+def resume(request):
+	context = {'resume':'active'}
+	return render(request, 'core/resume.html', context)
+
+def github(request):
+	context = {'github':'active'}
+	return render(request, 'core/github.html', context)
+
+
+def certi(request):
+	context = {'certi':'active'}
+	return render(request, 'core/accomplishments.html', context)
+
 
 def contact(request):
 	context = {'contact':'active'}
